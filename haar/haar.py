8be98b2d013419a2c3381232b40b3008d21517dd
Hrishikesh Barua <talonx@gmail.com>
2011-07-02 15:34:30 +0530

    Changes in haar

--- haar/haar.py
+++ haar/haar.py
@@ -1,35 +1,35 @@
 import sys
-from math import log
+from math import log,fabs
 
 def haar(args):
-	args = [int(i) for i in args[1:]]
-	if len(args) % 2 != 0:
-		print "Odd # of args", len(args)
-		return
-	
 	ga = average(args) #global average
-
+	print "Global average", ga
 	steps = int(log(len(args), 2))
 	print "Steps required", steps
 
-	res = process(args)
-
+	r = len(args)
+	res = process(args, r)
+	r = r/2
 	for i in range(steps - 1):
-		res = process(res)
+		print i
+		res = process(res, r)
+		r = r/2
+	return res
 
-	print res
-
-def process(args):
+def process(args, r):
 	res = []
-	for i in range(0, len(args), 2):
+	for i in range(0, r, 2):
 		res.append(av(args[i], args[i + 1]))
 
 	j = 0
-	for i in range(0, len(args), 2):
+	for i in range(0, r, 2):
 		res.append(args[i] - res[j])
 		j += 1
 
-	return res
+	if r == len(args):
+		return res
+	else:
+		return res + args[r:]
 
 def average(nums):
 	sum = 0;
@@ -41,7 +41,12 @@ def av(i, j):
 	return (i+j)/2.0 #http://python-history.blogspot.com/2009/03/problem-with-integer-division.html
 
 def main():
-	haar(sys.argv)
+	args = [int(i) for i in sys.argv[1:]]
+	if len(args) % 2 != 0:
+		print "Odd # of args", len(args)
+		return
+	res = haar(args)
+	print(res)
 
 if __name__ == '__main__':
 	main()
